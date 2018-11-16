607a86ba0aedaba96aa7f8434edcda8bc35fadf0
niravs200 <niravs200@gmail.com>
2018-11-16 17:23:04 -0400

    Fetch Coordinates and Draw Route

---
+++ AndroidStudioProjects/RouteDisplay/app/src/main/java/com/example/nirav/routedisplay/MainActivity.java
@@ -0,0 +1,170 @@
+package com.example.nirav.routedisplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import android.location.Location;
+import android.widget.Toast;
+import android.support.annotation.NonNull;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener{
+
+
+    private MapView mapView;
+    private MapboxMap mapbox;
+    private PermissionsManager permissionsManager;
+    private Location originLocation;
+    private NavigationMapRoute navigationMapRoute;
+    private DirectionsRoute currentRoute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this,getString(R.string.access_token));
+        setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        this.mapbox = mapboxMap;
+        enableOriginLocation();
+        enableDestinationLocation();
+        getRoute();
+    }
+
+    private void enableOriginLocation() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            initializeLocationComponent();
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    private void enableDestinationLocation()
+    {
+        mapbox.addMarker(new MarkerOptions().setPosition(new LatLng(44.6407,-63.5783)));
+    }
+
+    private void getRoute(){
+        NavigationRoute.builder(this)
+                .accessToken(Mapbox.getAccessToken())
+                .origin(Point.fromLngLat(originLocation.getLongitude(),originLocation.getLatitude()))
+                .destination(Point.fromLngLat(44.6407,-63.5783))
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                        Toast.makeText(MainActivity.this,"No route found",Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void initializeLocationComponent()
+    {
+        LocationComponent locationComponent = mapbox.getLocationComponent();
+        locationComponent.activateLocationComponent(this);
+        locationComponent.setRenderMode(RenderMode.GPS);
+        locationComponent.setCameraMode(CameraMode.TRACKING_GPS);
+        locationComponent.setLocationComponentEnabled(true);
+        originLocation = locationComponent.getLastKnownLocation();
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            onMapReady(mapbox);
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode,permissions,grantResults);
+    }
+
+    @Override
+    @SuppressWarnings( {"MissingPermission"})
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+}
